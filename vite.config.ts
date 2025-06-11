f15cd5f986842a41a39141f17ece95d816e53881
thowfeeqahmad <thowfeeqahmad687@gmail.com>
2025-06-11 19:53:51 +0530

    vite config modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/devbee/',  // ✅ correct since repo name is devbee
   plugins: [react()],
-  base: '/devbee/', // 👈 This is essential for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
